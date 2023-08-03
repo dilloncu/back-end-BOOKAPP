682e3de22d9e25f2d9a7b5fe81017ebf8ff2c8ae
dilloncu <dilloncushman9@gmail.com>
2023-08-03 16:28:06 +0100

    testing testing

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-
-
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
-
 
 const app = express();
 app.use(cors());
@@ -12,18 +9,23 @@ app.use(cors());
 const PORT = process.env.PORT || 8099;
 const Book = require("./models/Books");
 mongoose.connect(process.env.DATABASE_URL);
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
+  response.status(200).json("test request received");
+});
 
-  response.status(200).json('test request received')
-
-})
-
-app.get("/book", async (request,response)=>{
+app.get("/book", async (request, response) => {
   const allBooks = await Book.find(request.query);
   console.log("I do work");
   response.status(200).json(allBooks);
-  
+});
 
+app.post("/book", async (request, response) => {
+  try {
+    const newBook = await Book.create(request.body);
+    response.status(200).json(newBook);
+  } catch (error) {
+    response.status(500).json(error);
+  }
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
